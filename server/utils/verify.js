487ef9bd4dd9fd30a67fbd243009b7eb2af8c29d
keerthiprasath3801 <113460067+keerthiprasath3801@users.noreply.github.com>
2025-02-11 20:41:56 +0530

    Update verify.js

--- server/utils/verify.js
+++ server/utils/verify.js
@@ -4,9 +4,7 @@ import { createError } from "./error.js";
 export const verifyToken = (req, res, next) => {
     const token = req.cookies?.access_token;
     console.log(token);
-    if(!token){
-        return next(createError(401,"Not Authenticated!"))
-    }
+   
     jwt.verify(token,process.env.JWT,(err,user) => {
         if (err) return next(createError(403,'Token is not valid'));
         req.user = user;
